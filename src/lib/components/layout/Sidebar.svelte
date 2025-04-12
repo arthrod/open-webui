2cf2a9b79fc1c1b785387220b54b58ed5c95b051
shadowfax <theshadowfaxproject@gmail.com>
2025-04-12 02:10:09 -0700

    open-webui-66 Changed tooltip 'New Chat' to 'New Gift Chat'

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -500,7 +500,7 @@
 						<path
 							stroke-linecap="round"
 							stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+ 							d="M15.75 19.5 8.25 12l7.5-7.5"
 						/>
 					</svg>
 				</div>
